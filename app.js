7e5e02048f11fd1da65a821bba0e25c724892f23
steven <steven.h@live.com>
2021-03-13 17:15:53 -0800

    add ability to toggle form with button and to display books in library with a toggle read status btn and delete book btn

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+const newBook = document.querySelector('#new-book');
+const newBookButton = newBook.querySelector('button');
+
+const newBookForm = newBook.querySelectorAll('#new-book-form');
+newBookForm[0].style.display = 'none';
+
+const newBookFormSubmitBtn = newBookForm[0].querySelector('#new-book-submit-btn');
+
+const displayBooksDiv = document.querySelector('#display-books');
+const displayBooksBtn = displayBooksDiv.querySelector('button');
+
 let library = [];
+
 
 class Book {
   constructor(title, author, pages, read) {
@@ -8,7 +20,7 @@ class Book {
     this.read = read;
 
     this.info = () => {
-      return `${this.title} by ${this.author}, ${this.pages}, ${read ? 'has read' : 'not read yet'}.`;
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${read === 'true' ? 'has read' : 'not read yet'}.`;
     };
   }
 }
@@ -18,22 +30,35 @@ const addBookToLibrary = book => {
 };
 
 const displayBooks = () => {
-  for (book in library) {
-    console.log(book.info);
-  }
+  library.forEach((book, idx) => {
+    const bookDiv = document.createElement('DIV');
+    bookDiv.setAttribute('data-index', idx);
+
+    let info = document.createTextNode(book.info());
+
+    const deleteBtn = document.createElement('BUTTON');
+    deleteBtn.appendChild(document.createTextNode('DELETE BOOK'));
+
+    const readBtn = document.createElement('BUTTON');
+    readBtn.appendChild(document.createTextNode('TOGGLE READ'));
+
+    bookDiv.appendChild(info);
+    bookDiv.appendChild(document.createElement('BR'));
+    bookDiv.appendChild(readBtn)
+    bookDiv.appendChild(deleteBtn);
+
+    displayBooksDiv.appendChild(bookDiv);
+
+    deleteBtn.addEventListener('click', () => library.splice(idx, 1));
+    readBtn.addEventListener('click', () => library[idx].read === 'true' ? library[idx].read = 'false' : library[idx].read = 'true');
+  });
 };
-
-const newBook = document.querySelector('#new-book');
-const newBookButton = newBook.querySelector('button');
-const newBookForm = document.querySelector('#new-book-form');
-newBookForm.style.display = 'none';
-
 const showForm = () => {
-  if (newBookForm.style.display === 'none') {
-    newBookForm.style.display = 'block';
+  if (newBookForm[0].style.display === 'none') {
+    newBookForm[0].style.display = 'block';
   }
   else {
-    newBookForm.style.display = 'none';
+    newBookForm[0].style.display = 'none';
   }
 };
 
@@ -41,3 +66,18 @@ newBookButton.addEventListener('click', e => {
   console.log(e);
   showForm();
 });
+
+newBookFormSubmitBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  let fd = new FormData(document.forms[0]);
+  addBookToLibrary(new Book(fd.get('title'),
+    fd.get('author'),
+    fd.get('pages'),
+    fd.get('read'))
+  );
+
+  document.forms[0].reset();
+});
+
+displayBooksBtn.addEventListener('click', () => displayBooks());
